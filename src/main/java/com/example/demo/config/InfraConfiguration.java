05b0fdb93bc88fe71407d6994f4ff960b4c764fd
jsjg73 <kjs930703@naver.com>
2021-08-03 16:47:00 +0900

    Feat: BusInfra 빈 등록

--- src/main/java/com/example/demo/config/InfraConfiguration.java
+++ src/main/java/com/example/demo/config/InfraConfiguration.java
@@ -1,8 +1,20 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.mycom.navigation.bus.factory.BusInfra;
+import com.mycom.navigation.bus.factory.BusInfraFactory;
+import com.mycom.navigation.bus.reader.BusInfraReader;
+import com.mycom.navigation.bus.reader.BusInfraTxtReader;
 
 @Configuration
 public class InfraConfiguration {
 	
+	@Bean
+	public BusInfra businfra(BusInfraFactory factory) {
+		BusInfraReader bifReader = new BusInfraTxtReader();
+		BusInfra bif =  new BusInfraFactory(bifReader).createOnlyOnce();
+		return bif;
+	}
 }
