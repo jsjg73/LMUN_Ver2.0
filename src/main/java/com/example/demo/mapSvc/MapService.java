a75d8b9a1fb6bcf06fbddfef56acf7e77ecfadd7
jsjg73 <kjs930703@naver.com>
2021-08-03 22:05:25 +0900

    Feat: mapService 클래스 길찾기 메서드 추가

--- src/main/java/com/example/demo/mapSvc/MapService.java
+++ src/main/java/com/example/demo/mapSvc/MapService.java
@@ -2,9 +2,12 @@ package com.example.demo.mapSvc;
 
 import java.util.List;
 
+import com.example.demo.RealPathOriginDestinationVO;
 import com.example.demo.dto.Place;
 
 public interface MapService {
 
 	public List<Place> recommendPlaces(List<Place> places);
+
+	public List<Double[][]> searchRealPath(List<RealPathOriginDestinationVO> vos);
 }
--- src/main/java/com/example/demo/mapSvc/MapServiceImpl.java
+++ src/main/java/com/example/demo/mapSvc/MapServiceImpl.java
@@ -1,18 +1,31 @@
 package com.example.demo.mapSvc;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.RealPathOriginDestinationVO;
 import com.example.demo.dto.Place;
 import com.example.demo.kakao.KakaoMapServiceImpl;
+import com.mycom.navigation.bus.navi.DijkstraNavigator;
+import com.mycom.navigation.bus.navi.Navigator;
+import com.mycom.navigation.infra.Infrastructure;
 
 @Service
 public class MapServiceImpl implements MapService {
 
-	@Autowired
 	private KakaoMapServiceImpl kms;
+	private Infrastructure infra;
+	
+	
+	public MapServiceImpl(KakaoMapServiceImpl kms, Infrastructure infra) {
+		super();
+		this.kms = kms;
+		this.infra = infra;
+	}
+
 
 	// 중심 좌표 구하기.
 	// 출발지 좌표 값의 평균.
@@ -37,6 +50,26 @@ public class MapServiceImpl implements MapService {
 		Place center = getCenter(places);
 		
 		return kms.recommendPlaces(center);
+	}
+
+
+	@Override
+	public List<Double[][]> searchRealPath(List<RealPathOriginDestinationVO> vos) {
+		List<Double[][]> realPaths = new ArrayList<Double[][]>();
+		Double[][] paths = new Double[vos.size()][];
+		Navigator navi = new DijkstraNavigator(infra);
+		
+		for(RealPathOriginDestinationVO vo : vos) {
+			
+			List<Double[]> list = navi.navigate(
+					Double.parseDouble(vo.getSx()),
+					Double.parseDouble(vo.getSy()),
+					Double.parseDouble(vo.getEx()),
+					Double.parseDouble(vo.getEy())
+					);
+			realPaths.add( list.toArray(new Double[0][0]));
+		}
+		return realPaths;
 	}
 	
 	
